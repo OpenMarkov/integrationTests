e253bce93ad9734e9dd8305e7f58efb923e631c4
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-07-02 22:26:40 +0200

    [Feature] Opened all modules to allow reflections to be used across all modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+open module org.openmarkov.integrationTests {
+    requires org.jetbrains.annotations;
+    requires org.json;
+    requires org.openmarkov.annotation_processing;
+    requires org.openmarkov.core;
+    requires org.openmarkov.costeffectiveness;
+    requires org.openmarkov.dbgenerator;
+    requires org.openmarkov.full;
+    requires org.openmarkov.gui;
+    requires org.openmarkov.inference;
+    requires org.openmarkov.io;
+    requires org.openmarkov.io.database.elvira;
+    requires org.openmarkov.io.database.excel;
+    requires org.openmarkov.io.database.weka;
+    requires org.openmarkov.learning.algorithm;
+    requires org.openmarkov.learning.core;
+    requires org.openmarkov.learning.gui;
+    requires org.openmarkov.learning.metric;
+    requires org.openmarkov.sensitivityanalysis;
+    requires org.openmarkov.stochasticpropagationoutput;
+    requires java.xml;
+    
+    exports org.openmarkov.integrationTests;
+    exports org.openmarkov.integrationTests.localize;
+}
