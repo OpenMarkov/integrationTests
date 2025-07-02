50eb1f935bfa013c4dff90920e7f5ef94878575e
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-07-02 22:38:24 +0200

    [Test] Added a test validate Strings written on ClassLocalization XML files can be properly used at runtime.

---
+++ src/test/java/org/openmarkov/integrationTests/integrationTests/localization/autolocalization/ValidateAutoLocalization.java
@@ -0,0 +1,311 @@
+package org.openmarkov.integrationTests.integrationTests.localization.autolocalization;
+
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.Test;
+import org.openmarkov.core.localize.AutoLocalizable;
+import org.openmarkov.core.localize.StringBundle;
+import org.openmarkov.core.localize.StringDatabase;
+import org.openmarkov.core.localize.spi.LocalizeResourcesProvider;
+import org.openmarkov.core.stringformat.StringFormat;
+import org.openmarkov.plugin.Filter;
+import org.openmarkov.plugin.PluginLoader;
+
+import java.lang.reflect.Modifier;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * See the method {@link ValidateAutoLocalization#validateAutoLocalization()}, which is the purpose of this test class.
+ *
+ * @author jrico
+ */
+public class ValidateAutoLocalization {
+    
+    /**
+     * List of all {@link Bundle}, including their {@link StringBundle} preloaded.
+     */
+    private static final List<Bundle> BUNDLES = StringDatabase
+            .getBundleProviders()
+            .flatMap(Bundle::of)
+            .toList();
+    /**
+     * Map where every module has a list of {@link Bundle}s that are defined in said module.
+     */
+    private static final Map<Module, List<Bundle>> MODULES_AND_BUNDLES = ValidateAutoLocalization.BUNDLES
+            .stream()
+            .collect(Collectors.groupingBy(bundle -> bundle.provider.getClass().getModule()));
+    /**
+     * Map where every module has a list of {@link AutoLocalizable}s that are defined in said module.
+     */
+    private static final Map<Module, List<Class<AutoLocalizable>>> MODULES_AND_AUTOLOCALIZABLES = new PluginLoader()
+            .loadAllPlugins(Filter.filter().toImplement(AutoLocalizable.class))
+            .stream()
+            .map(localizableClass -> (Class<AutoLocalizable>) localizableClass)
+            .filter(localizableClass -> !localizableClass.isInterface() && !Modifier.isAbstract(localizableClass.getModifiers()))
+            .collect(Collectors.groupingBy(Class::getModule));
+    /**
+     * List where every {@link AutoLocalizable} of every module is associated to the Bundles that can be accessed in
+     * said module.
+     */
+    private static final List<AutolocalizablesAndBundles> LOCALIZABLES_AND_ACCESIBLE_BUNDLES = ValidateAutoLocalization.MODULES_AND_AUTOLOCALIZABLES
+            .entrySet()
+            .stream()
+            .map(moduleAndLocalizable -> {
+                var targetModule = moduleAndLocalizable.getKey();
+                List<Bundle> resourceProviders = ValidateAutoLocalization.MODULES_AND_BUNDLES
+                        .entrySet()
+                        .stream()
+                        .filter(providersModule -> targetModule.canRead(providersModule.getKey()))
+                        .flatMap(providersModule -> providersModule.getValue()
+                                                                   .stream())
+                        .toList();
+                return new AutolocalizablesAndBundles(resourceProviders, moduleAndLocalizable.getValue());
+            }).toList();
+    
+    /**
+     * Searches for every {@link AutoLocalizable} class and then gets their localization String in the
+     * {@link StringDatabase#getUniqueInstance()} to verify it is well written.
+     * <p>
+     * A localization String is well written when:
+     * <ul>
+     *   <li>A localization String is specified for the class.</li>
+     *   <li>The localization String is written in a stringBundle that is accessible to the class.</li>
+     *   <li>All the values (See {@link StringFormat}) of the localization String correspond to fields of the class.
+     *   </li>
+     *   <li>All the pseudocodes written on the values can be resolved.
+     *   </li>
+     * </ul>
+     * If a localization String was to be wrong, this test will fail specifying which where wrong, why, and it will also
+     * print some advice and example code on how to solve it.
+     */
+    @Test void validateAutoLocalization() {
+        List<Error> errors = new ArrayList<>();
+        ValidateAutoLocalization.LOCALIZABLES_AND_ACCESIBLE_BUNDLES
+                .stream()
+                .flatMap(autolocalizablesAndBundles ->
+                                 autolocalizablesAndBundles.localizablesClasses
+                                         .stream()
+                                         .map(autoLocalizableClass -> new AutolocalizableAndBundles(autolocalizablesAndBundles.bundles, autoLocalizableClass))
+                )
+                .forEach(autolocalizableAndBundles -> {
+                    Class<AutoLocalizable> localizableClasses = autolocalizableAndBundles.localizablesClasses;
+                    String keyName = localizableClasses.getName();
+                    @Nullable Bundle localizedBundle;
+                    Optional<Bundle> accessibleBundle = autolocalizableAndBundles.bundles
+                            .stream()
+                            .filter(bundles -> bundles.stringBundle.getString(keyName) != null)
+                            .findFirst();
+                    if (accessibleBundle.isPresent()) {
+                        localizedBundle = accessibleBundle.get();
+                    } else {
+                        Optional<Bundle> inaccessibleBundle = ValidateAutoLocalization.BUNDLES.stream()
+                                                                                              .filter(bundles -> bundles.stringBundle.getString(keyName) != null)
+                                                                                              .findFirst();
+                        if (inaccessibleBundle.isPresent()) {
+                            localizedBundle = inaccessibleBundle.get();
+                            errors.add(new Error.LocalizationInInaccessibleBundle(localizableClasses, localizedBundle));
+                        } else {
+                            errors.add(new Error.LocalizationMissing(localizableClasses));
+                            return;
+                        }
+                    }
+                    String localizedString = localizedBundle.stringBundle.getString(keyName);
+                    var formattings = StringFormat.getAllFormattings(localizedString).toList();
+                    for (var formatting : formattings) {
+                        Class<?> argumentClass = localizableClasses;
+                        if (!"this".equals(formatting.field())) {
+                            var fieldClass = new StringFormat.PseudoCode(StringFormat.PseudoCode.Marker.FIELD, formatting.field())
+                                    .resolveClassesThatShouldBeOpen(argumentClass);
+                            if (!fieldClass.found()) {
+                                errors.add(new Error.FieldOrMethodMissing(localizableClasses, localizableClasses, formatting.field(), StringFormat.PseudoCode.Marker.FIELD));
+                                continue;
+                            }
+                        }
+                        for (var pseudocode : formatting.pseudocode()) {
+                            var classesFound = pseudocode.resolveClassesThatShouldBeOpen(argumentClass);
+                            if (classesFound.found()) {
+                                argumentClass = classesFound.value().get(classesFound.value().size() - 1);
+                            } else {
+                                errors.add(new Error.FieldOrMethodMissing(localizableClasses, argumentClass, pseudocode.methodOrAttributeName(), pseudocode.marker()));
+                            }
+                        }
+                    }
+                });
+        
+        
+        if (!errors.isEmpty()) {
+            var errorClassAndErrors = errors.stream().collect(Collectors.groupingBy(Error::getClass));
+            String errorsDescription = errorClassAndErrors
+                    .entrySet()
+                    .stream()
+                    .map(entry -> {
+                        var errorClass = entry.getKey();
+                        var unspecifiedErrorsForClass = entry.getValue();
+                        if (errorClass == Error.FieldOrMethodMissing.class) {
+                            var errorsForClass = unspecifiedErrorsForClass
+                                    .stream().map(v -> (Error.FieldOrMethodMissing) v)
+                                    .sorted(Comparator.comparing(v -> v.classWithMissingComponent.getName()))
+                                    .toList();
+                            String subErrorsDetails = errorsForClass.stream()
+                                                                    .map(error ->
+                                                                                 "\t- No " + error.marker.toString()
+                                                                                                         .toLowerCase() + " "
+                                                                                         + error.missingComponent + " in "
+                                                                                         + error.classWithMissingComponent.getName() + " of module "
+                                                                                         + error.classWithMissingComponent.getModule()
+                                                                                                                          .getName())
+                                                                    .distinct()
+                                                                    .collect(Collectors.joining(System.lineSeparator()));
+                            return "Some fields and methods are missing:" + System.lineSeparator() + subErrorsDetails;
+                        }
+                        if (errorClass == Error.LocalizationMissing.class) {
+                            var errorsForClass = unspecifiedErrorsForClass
+                                    .stream().map(v -> (Error.LocalizationMissing) v)
+                                    .sorted(Comparator.comparing(v -> v.localizableClass.getName()))
+                                    .toList();
+                            String subErrorsDetails = errorsForClass.stream()
+                                                                    .map(error ->
+                                                                                 "    <Localization class=\"" + error.localizableClass.getName()
+                                                                                         + "\"" + System.lineSeparator() +
+                                                                                         "                  value=\"\"/>")
+                                                                    .distinct()
+                                                                    .collect(Collectors.joining(System.lineSeparator()));
+                            return "Some classes are not localized, you can add them with:" + System.lineSeparator() + subErrorsDetails;
+                        }
+                        if (errorClass == Error.LocalizationInInaccessibleBundle.class) {
+                            var errorsForClass = unspecifiedErrorsForClass
+                                    .stream().map(v -> (Error.LocalizationInInaccessibleBundle) v)
+                                    .sorted(Comparator.comparing(v -> v.localizableClass.getName()))
+                                    .toList();
+                            String subErrorsDetails = errorsForClass.stream()
+                                                                    .map(error ->
+                                                                                 "\tClass " + error.localizableClass.getName() + " is localized in " + error.wrongBundle.stringBundle + "_en.xml of module " + error.wrongBundle.provider.getClass()
+                                                                                                                                                                                                                                         .getModule()
+                                                                                                                                                                                                                                         .getName() + ", but it should be in " + error.localizableClass.getModule()
+                                                                                                                                                                                                                                                                                                       .getName())
+                                                                    .distinct()
+                                                                    .collect(Collectors.joining(System.lineSeparator()));
+                            return "Some classes are localized in the wrong module, you can apply this movements:" + System.lineSeparator() + subErrorsDetails;
+                        }
+                        return "";
+                    })
+                    .collect(Collectors.joining(System.lineSeparator() + System.lineSeparator()));
+            fail(errorsDescription);
+        }
+        
+    }
+    
+    /**
+     * Represents every kind of error related to an improper localization of a {@link AutoLocalizable} class.
+     */
+    public static abstract sealed class Error permits Error.FieldOrMethodMissing, Error.LocalizationInInaccessibleBundle, Error.LocalizationMissing {
+        
+        /**
+         * Represents the error of an {@link AutoLocalizable} class that has no localization file.
+         */
+        static final class LocalizationMissing extends Error {
+            Class<AutoLocalizable> localizableClass;
+            
+            /**
+             * Constructs a {@code LocalizationMissing} error with the specified {@link AutoLocalizable} class.
+             *
+             * @param localizableClass the class that is missing localization.
+             */
+            LocalizationMissing(Class<AutoLocalizable> localizableClass) {
+                this.localizableClass = localizableClass;
+            }
+        }
+        
+        /**
+         * Represents the error of an {@link AutoLocalizable} class whose localization value is in a stringBundle not
+         * accessible from this class.
+         */
+        static final class LocalizationInInaccessibleBundle extends Error {
+            Class<AutoLocalizable> localizableClass;
+            Bundle wrongBundle;
+            
+            /**
+             * Constructs an error representing an {@link AutoLocalizable} class whose localization value is in a stringBundle
+             * not accessible from this class.
+             *
+             * @param localizableClass the class whose localization is in an inaccessible stringBundle.
+             * @param wrongBundle      the stringBundle containing localization value, but that is not accessible to the class.
+             */
+            LocalizationInInaccessibleBundle(Class<AutoLocalizable> localizableClass, Bundle wrongBundle) {
+                this.localizableClass = localizableClass;
+                this.wrongBundle = wrongBundle;
+            }
+        }
+        
+        /**
+         * Represents the error of a field or method that cannot be resolved reflectively when resolving the
+         * localization String of the {@link AutoLocalizable} class.
+         */
+        static final class FieldOrMethodMissing extends Error {
+            Class<AutoLocalizable> whenLocalizingClass;
+            Class<?> classWithMissingComponent;
+            String missingComponent;
+            StringFormat.PseudoCode.Marker marker;
+            
+            /**
+             * Constructs the error to represent a field or method that could not be resolved while resolving the
+             * localization String.
+             *
+             * @param whenLocalizingClass       the {@link AutoLocalizable} class being localized.
+             * @param classWithMissingComponent the class containing the missing field or method.
+             * @param missingComponent          the name of the missing field or method.
+             * @param marker                    the marker associated with the formatting process.
+             */
+            FieldOrMethodMissing(Class<AutoLocalizable> whenLocalizingClass, Class<?> classWithMissingComponent, String missingComponent, StringFormat.PseudoCode.Marker marker) {
+                this.whenLocalizingClass = whenLocalizingClass;
+                this.classWithMissingComponent = classWithMissingComponent;
+                this.missingComponent = missingComponent;
+                this.marker = marker;
+            }
+        }
+    }
+    
+    /**
+     * List of {@link AutoLocalizable} classes associated with the bundles they can access (Meaning these
+     * {@link AutoLocalizable}s come all from the same module).
+     */
+    private record AutolocalizablesAndBundles(List<Bundle> bundles,
+                                              List<Class<AutoLocalizable>> localizablesClasses) {
+    }
+    
+    /**
+     * A {@link AutoLocalizable} class associated with the stringBundle it can access.
+     */
+    private record AutolocalizableAndBundles(List<Bundle> bundles,
+                                             Class<AutoLocalizable> localizablesClasses) {
+    }
+    
+    /**
+     * Aggregate of all the information to represent a Bundle file.
+     *
+     * @param provider     The {@link LocalizeResourcesProvider} where this stringBundle came from.
+     * @param bundleName   The name of the stringBundle.
+     * @param stringBundle The resolved {@link StringBundle}.
+     */
+    public record Bundle(LocalizeResourcesProvider provider, String bundleName, StringBundle stringBundle) {
+        
+        /**
+         * Creates a stream of bundles created from every Bundle in
+         * {@link LocalizeResourcesProvider#getBundlesMap(Locale)}.
+         *
+         * @param provider Used to retrieve all the localization resource bundles.
+         * @return a stream of bundles created from every Bundle in
+         * {@link LocalizeResourcesProvider#getBundlesMap(Locale)}.
+         */
+        public static Stream<Bundle> of(LocalizeResourcesProvider provider) {
+            return provider.getBundlesMap(Locale.ENGLISH)
+                           .entrySet()
+                           .stream()
+                           .map(entry -> new Bundle(provider, entry.getKey(), entry.getValue()));
+        }
+    }
+    
+}
