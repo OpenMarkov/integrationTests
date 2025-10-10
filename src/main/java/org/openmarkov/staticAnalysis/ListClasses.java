907cb3053ce587f4a7c5266a89bc5f3aafe6f2cb
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2025-10-10 13:27:37 +0200

    [Refactor] Now edits check constraints instead of constraints checks edits.

--- src/main/java/org/openmarkov/staticAnalysis/ListClasses.java
+++ src/main/java/org/openmarkov/staticAnalysis/ListClasses.java
@@ -1,21 +1,21 @@
 package org.openmarkov.staticAnalysis;
 
-import org.openmarkov.core.action.AddFindingEdit;
-import org.openmarkov.core.action.PNEdit;
 import org.openmarkov.core.model.network.constraint.PNConstraint;
-import org.openmarkov.gui.toolplugin.ToolPlugin;
 import org.openmarkov.plugin.PluginSearch;
 
 import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ListClasses {
     
     public static void main(String[] args) {
+        AtomicInteger index = new AtomicInteger();
+        
         PluginSearch.init()
                     .childrenOf(PNConstraint.class)
                     .stream()
                     .sorted(Comparator.comparing(Class::getName))
-                    .forEach(System.out::println);
+                    .forEach(classToPrint -> System.out.println(index.incrementAndGet() + " - " + classToPrint.getName()));
     }
     
 }
