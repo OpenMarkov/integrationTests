2a785236843f92b4fa75481dc3c9a7e4a3b57643
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2026-01-26 21:03:23 +0100

    [Feature] The exception for 'Potential Type not supported' now also appears when the 'autoGetPotential' function cannot find a 'Method' to generate the Potential.

--- src/main/java/org/openmarkov/staticAnalysis/PrintClassExtensionTree.java
+++ src/main/java/org/openmarkov/staticAnalysis/PrintClassExtensionTree.java
@@ -10,18 +10,11 @@ import java.util.Comparator;
 public class PrintClassExtensionTree {
     
     public static void main(String[] args) {
-        var search = PluginSearch.init().childrenOf(Potential.class).filter(ClassUtils::isConcrete).extensionTree();
-        search.print();
-        //new org.openmarkov.core.model.network.potential.AugmentedProbTablePotential(List.of(), PotentialRole.JOINT_PROBABILITY);
-        search.breathFirstLevelOrderQueue()
-              .reversed()
-              .stream()
-              .map(ExtensionTree::getCurrentClass)
-              .filter(ClassUtils::isConcrete)
-              .sorted(Comparator.comparing(Class::getName))
-              .forEach(System.out::println);
-        
-        
+        PluginSearch.init()
+                    .childrenOf(Potential.class)
+                    .filter(ClassUtils::isConcrete)
+                    .extensionTree()
+                    .print();
     }
     
 }
